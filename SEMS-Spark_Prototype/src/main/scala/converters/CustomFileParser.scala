13e83a0568c3171be236c23a4ef7a71a95499e9d
Jacob Heldenbrand <jacobrh91@gmail.com>
2017-10-09 11:23:36 -0500

    initial commit

---
+++ SEMS-Spark_Prototype/src/main/scala/converters/CustomFileParser.scala
@@ -0,0 +1,22 @@
+package converters
+
+import java.io.File
+
+class CustomFileParser(filePath: File,
+                       delimiter: String = "\\t",
+                       columnsToDelete: Seq[Int] = Seq(),
+                       transpose: Boolean = false
+                      ) 
+        extends FileParser(filePath) {
+  
+  private val table = this.readFile(filePath, delimiter)
+  
+  private val convertedTable = {
+    val filtered = table.deleteColumns(columnsToDelete: _*)
+    if (transpose) filtered.transpose else filtered
+  }
+  
+  def saveParsedFile(outputPath: File) = {
+    convertedTable.replaceTopLeftEntry.saveTableAsTSV(outputPath)
+  }
+}
