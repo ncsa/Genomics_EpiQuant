cbb298462db2c1eea310133d82c586166b1c7f37
sstevenshang <sstevenshang@gmail.com>
2018-11-05 14:07:07 -0600

    temp commit

--- SPAEML/src/main/scala/Main.scala
+++ SPAEML/src/main/scala/Main.scala
@@ -1,21 +1,34 @@
+import lasso.LASSO.train
+import org.apache.spark.sql.SparkSession
+
 object Main {
 
   def main(args: Array[String]) {
 
-    def usage(): Unit = {
-      println("Usage: program [ConvertFormat | StepwiseModelSelection] [options]")
-    }
+    val spark = SparkSession
+      .builder
+      .master("local")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate
 
-    if (args.length == 0) usage
-    else {
+    train("/Users/stevenshang/genomics/SPAEML/src/test/resources/Genotypes/10.Subset.n=300.epiq",
+      "/Users/stevenshang/genomics/SPAEML/src/test/resources/Phenotypes/Simulated.Data.1.Reps.Herit.0.92_n=300.epiq",
+      spark.sparkContext)
 
-      val name = args(0)
-      name match {
-        case "ConvertFormat" => ConvertFormat.launch(args.drop(1))
-        case "StepwiseModelSelection" => StepwiseModelSelection.launch(args.drop(1))
-        case _ => usage
-      }
-    }
+//    def usage(): Unit = {
+//      println("Usage: program [ConvertFormat | StepwiseModelSelection] [options]")
+//    }
+//
+//    if (args.length == 0) usage
+//    else {
+//
+//      val name = args(0)
+//      name match {
+//        case "ConvertFormat" => ConvertFormat.launch(args.drop(1))
+//        case "StepwiseModelSelection" => StepwiseModelSelection.launch(args.drop(1))
+//        case _ => usage
+//      }
+//    }
   }
   
 }
--- SPAEML/src/main/scala/lasso/LASSO.scala
+++ SPAEML/src/main/scala/lasso/LASSO.scala
@@ -1,13 +1,44 @@
 package lasso
 
+import org.apache.spark.SparkContext
 import org.apache.spark.mllib.regression.LassoWithSGD
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD
+import spaeml.{DenseFileData, SPAEMLDense}
 
-class LASSO {
+object LASSO {
 
-  def train(): Unit = {
-    
+  def train(genoTypeFile: String, phenoTypeFile: String, spark: SparkContext): Unit = {
+
+    val geno = SPAEMLDense.readHDFSFile(genoTypeFile, spark)
+    val pheno = SPAEMLDense.readHDFSFile(phenoTypeFile, spark)
+
+    val rdd = createRDD(geno, pheno, spark)
+    val model = LassoWithSGD.train(rdd, 100)
+
+    val valuesAndPreds = rdd.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }.collect()
+
+    println(rdd.count())
+    println(geno.dataPairs.size)
+    println(pheno.dataPairs.size)
+    println(valuesAndPreds.mkString)
+  }
+
+  def createRDD(geno: DenseFileData, pheno: DenseFileData, spark: SparkContext): RDD[LabeledPoint] = {
+
+    // class DenseFileData(sampleNames: Vector[String], dataPairs: Vector[(String, DenseVector[Double])])
+
+    val zippedData = geno.
+
+    val labledPoints = zippedData.map {
+      row => LabeledPoint(row._1._2(0), Vectors.dense(row._2._2.toArray))
+    }
+
+    spark.parallelize(labledPoints).cache()
   }
 
 }
